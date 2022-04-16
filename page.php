32068286d756a4ef4b7b89c7cf86b58a3fb98819
yosrbs <91878143+yosrbs@users.noreply.github.com>
2022-04-16 22:25:02 +0200

    Add files via upload

---
+++ page.php
@@ -0,0 +1,80 @@
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Freech</title>
+    <link rel="shortcut icon" href="icon.png" type="image/x-icon">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css">
+
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="stylee.css">
+</head>
+<body>
+<div class="cont">
+<header>
+    <div class="logo">
+        <i class="fas fa-gamepad"></i>
+        <p>FREECH</p>
+    </div>
+    <ul class="navbar">
+                <a href="Logout.php" class="active">Log Out</a>
+            </ul>
+</header>
+
+<section class="features">
+    <div class="features-containers">
+        <div class="feature-box">
+            <div class="f-img">
+                <img scr="doodler-guy.png"/>
+            </div>
+            <div class="f-text">
+                <center>
+                <h1>Doodle Jump</h1>
+                <p>In Doodle Jump, the aim is to guide a four-legged creature called "The Doodler" up a never-ending series of platforms without falling.</p>
+                <a href="doodlejump.html" class="active">Play</a>
+                </center>   
+            </div>
+        </div>
+        <div class="feature-box">
+            <div class="f-img">
+                <img scr="snake.png"/>
+            </div>
+            <div class="f-text">
+               <center> <h1>Snake</h1>
+                <p>The player controls a square on a bordered plane. As it moves forward, it leaves a trail behind, resembling a moving snake.</p>
+                <a href="snake.html" class="active">Play</a></center>
+            </div>
+        </div>
+        <div class="feature-box">
+            <div class="f-img">
+                <img scr="tictactoe.png"/>
+            </div>
+            <div class="f-text">
+                <center>
+                <h1>Tic Tac Toe</h1>
+                <p>Tic-tac-toe is played on a three-by-three grid by two players, who alternately place the marks X and O in one of the nine spaces in the grid.</p>
+                <a href="tictactoe.html" class="active">Play</a>
+                </center>
+            </div>
+        </div>
+    </div>
+</section>
+</div>
+<div class="footer-bottom">
+        <p>Copyright &copy; All Right Reserved | Designed <span>By Freech Team</span> 2022</p>
+    </div>
+
+    
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
